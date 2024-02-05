95d1619a867d02735a148478232e121810c58e91
almak75 <111127658+almak75@users.noreply.github.com>
2024-02-05 10:37:28 +0300

    Add files via upload

---
+++ prod.py
@@ -0,0 +1,204 @@
+
+
+import random
+#import zipfile
+from copy import deepcopy
+#from pathlib import Path
+#import matplotlib.pyplot as plt
+#import numpy as np
+import torch
+from PIL import Image
+from torch import nn
+#from torch import optim
+
+import warnings
+warnings.filterwarnings('ignore', category=UserWarning)
+from torchvision import transforms
+#from torchvision import datasets
+
+import time
+#from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torchvision.transforms import v2
+import os
+#import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+#МОДЕЛЬ 1.
+#small 
+#обучена на 20000 на класса  256x256
+#точность 96.63 минимальный лосс 0.10854
+#классификатор
+#self.classifier = nn.Sequential(nn.Linear(self.embedding_size, 32), nn.ReLU(), nn.Dropout(0.1),  nn.Linear(32, 3)) #nn.Dropout(0.2),
+
+#модель 2.
+#small 
+#обучена на 3000 на класса  320х320
+#точность 95.06 минимальный лосс 0.15
+#классификатор
+#self.classifier = nn.Sequential(nn.Linear(self.embedding_size, 32), nn.ReLU(), nn.Dropout(0.1),  nn.Linear(32, 3)) #nn.Dropout(0.2),
+
+#модель 3.
+#BASE 
+#обучена на 20000 на класса  266х266
+#точность 97.09 минимальный лосс 0.08456  !!!!!!!!!!!!!!!!!!!!!!!
+#классификатор
+#self.classifier = nn.Sequential(nn.Linear(self.embedding_size, 256), nn.ReLU(), nn.Dropout(0.1),  nn.Linear(256, len(class_names))) 
+
+
+
+
+
+#device = 'cpu'
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print('ВНИМАНИЕ!!! Обрабатываеться будет всё на ',device, '<<<<<<<<<<<<<----------------')
+
+cls = {0:'hard',1 :'control', 2:'sexy'}
+dinov2 = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14_lc')
+
+
+from dinov2.models.vision_transformer import vit_small, vit_base #vit_large, vit_giant2
+class DinoVisionTransformerClassifier(nn.Module):
+
+    def __init__(self, model_size="small"):
+        super(DinoVisionTransformerClassifier, self).__init__()
+        self.model_size = model_size
+
+        # loading a model with registers
+        n_register_tokens = 4
+
+        if model_size == "small":
+            model = vit_small(patch_size=14,
+                              img_size=526,
+                              init_values=1.0,
+                              num_register_tokens=n_register_tokens,
+                              block_chunks=0)
+            self.embedding_size = 384
+            self.number_of_heads = 6
+            #model.load_state_dict(torch.load('models/dinov2_vits14_reg4_pretrain.pth',map_location=torch.device('cpu')))
+            self.transformer = deepcopy(model)
+            self.classifier = nn.Sequential(nn.Linear(self.embedding_size, 32), nn.ReLU(), nn.Dropout(0.1),  nn.Linear(32, 3)) #nn.Dropout(0.2),
+
+        elif model_size == "base":
+            model = vit_base(patch_size=14,
+                             img_size=526,
+                             init_values=1.0,
+                             num_register_tokens=n_register_tokens,
+                             block_chunks=0,
+                            #drop_path_rate=0.05
+                            )
+            self.embedding_size = 768
+            self.number_of_heads = 12
+            self.transformer = deepcopy(model)
+            self.classifier = nn.Sequential(nn.Linear(self.embedding_size, 256), nn.ReLU(), nn.Dropout(0.1),  nn.Linear(256, 3))
+
+            #model.load_state_dict(torch.load('models/dinov2_vitb14_reg4_pretrain.pth'))
+
+        
+
+        
+
+
+    def forward(self, x):
+        x = self.transformer(x)
+        x = self.transformer.norm(x)
+        x = self.classifier(x)
+        return x
+model1 = DinoVisionTransformerClassifier("small")
+#model2 = DinoVisionTransformerClassifier("small")
+model2 = DinoVisionTransformerClassifier("base")
+
+model1.load_state_dict(torch.load('model1.pth', map_location=torch.device(device)))
+#model2.load_state_dict(torch.load('model2.pth', map_location=torch.device(device)))
+model2.load_state_dict(torch.load('model3.pth', map_location=torch.device(device)))
+
+
+model1.eval()
+model2.eval()
+
+
+
+class ResizeAndPad:
+    def __init__(self, target_size, multiple):
+        self.target_size = target_size
+        self.multiple = multiple
+
+    def __call__(self, img):
+        # Resize the image
+        img = v2.Resize(self.target_size)(img)
+
+        # Calculate padding
+        pad_width = (self.multiple - img.width % self.multiple) % self.multiple
+        pad_height = (self.multiple - img.height % self.multiple) % self.multiple
+
+        # Apply padding
+        img = v2.Pad((pad_width // 2, pad_height // 2, pad_width - pad_width // 2, pad_height - pad_height // 2))(img)
+
+        return img
+
+
+
+inference_preprocessing1 = transforms.Compose([ ResizeAndPad(256, 14),
+                                               transforms.ToTensor(),
+                                               transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                                             ]
+                                            )
+inference_preprocessing2 = transforms.Compose([ ResizeAndPad(320, 14),
+                                               transforms.ToTensor(),
+                                               transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                                             ]
+                                            )
+
+inference_preprocessing3 = transforms.Compose([ ResizeAndPad(266, 14),
+                                               transforms.ToTensor(),
+                                               transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                                             ]
+                                            )
+
+
+def get_predict_from_model(img, model, preprocess):
+    img_tensor = preprocess(img)
+    img_tensor = img_tensor.unsqueeze(0)
+    input_tensor = img_tensor.to(device)
+    embeddings = model.transformer(input_tensor)
+    x = model.transformer.norm(embeddings)
+    output_tensor = model.classifier(x)
+    probabilities = torch.softmax(output_tensor, dim=1)
+    predicted_classes = torch.argmax(probabilities, dim=1)
+
+    c = int(predicted_classes[0])
+    
+    rez = dict(zip(cls.values(), probabilities.flatten().tolist()))
+    return  rez
+
+
+
+def look_to_file(img, param):
+    r1 = get_predict_from_model(img, model1, inference_preprocessing1)
+    r2 = get_predict_from_model(img, model2, inference_preprocessing3)
+    
+    #img = Image.open(file) 
+    #img_tensor = inference_preprocessing(img)
+    #img_tensor = img_tensor.unsqueeze(0)
+    #input_tensor = img_tensor.to(device)
+    #embeddings = model.transformer(input_tensor)
+    #x = model.transformer.norm(embeddings)
+    #output_tensor = model.classifier(x)
+    #probabilities = torch.softmax(output_tensor, dim=1)
+    #predicted_classes = torch.argmax(probabilities, dim=1)
+
+    #c = int(predicted_classes[0])
+    
+    #rez = dict(zip(cls.values(), probabilities.flatten().tolist()))
+    if (r1['sexy'] > 0.85) & (r2['sexy'] > 0.85):
+        need_moderation = 0
+    else:
+        need_moderation = 1
+
+    rez = {}
+    rez['need_moderation'] = need_moderation
+    rez['net1'] = r1
+    rez['net2'] = r2
+    
+    return rez
---
+++ prod_server.py
@@ -0,0 +1,128 @@
+#from fastapi import FastAPI, File, UploadFile,Form,  Depends
+from xml.dom import WrongDocumentErr
+from fastapi import FastAPI, File, UploadFile,Depends
+
+from typing import Optional
+from PIL import Image
+from io import BytesIO
+from pydantic import BaseModel#это понадобилось для втрого варианта
+#нужно для работы с файлами с айфона
+
+from pillow_heif import register_heif_opener
+
+from datetime import datetime
+import os
+
+#import imghdr
+#пробуем вставить логирование всех запросов, которые приходят на сервер
+import time
+from typing import Callable
+from fastapi import APIRouter, FastAPI, Request, Response
+from fastapi.routing import APIRoute
+
+import gc
+
+class TimedRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+        async def custom_route_handler(request: Request) -> Response:
+            before = time.time()
+            response: Response = await original_route_handler(request)
+
+     
+            current_datetime = datetime.now()
+            str_current_datetime = current_datetime.strftime("%Y-%m-%d %H-%M-%S")
+
+            duration = time.time() - before
+            response.headers["X-Response-Time"] = str(duration)
+          
+            #print(str_current_datetime, request.url.scheme, request.method, request.url.path, request.query_params, f'{request.client.host}:{request.url.port}', duration)
+            
+            return response
+        return custom_route_handler
+
+
+
+
+
+register_heif_opener()
+
+import prod #в этом  файле хранятся все процедуры
+
+app = FastAPI()
+router = APIRouter(route_class=TimedRoute) #ЭТО ДЛЯ РОУТЕРА
+#ПОЛУЧЕНИЕ ТОЛЬКО КАРТИНКИ. ОТВЕТ json
+keys = ['hard', 'control', 'sexy']
+wrong = {'need_moderation': 3, 'net1': {'hard': 0.0, 'control': 0.0, 'sexy': 0.0}, 'net2': {'hard': 0.0, 'control': 0.0, 'sexy': 0.0}}
+
+class Base2(BaseModel):
+    foto_id: Optional[int] = 1    #Требуется вернуть тут будут ID фото из внутренней базы
+    detect: Optional[int] = 1    #необходима детекция объектов
+    classify: Optional[int] = 1    #необходима классификация  изображений
+
+
+@router.post("/ps")
+async def analyze_image(base: Base2 = Depends(), image: UploadFile = File(...)):
+    #print('имя файла',image.filename)
+    #image_type = imghdr.what(image.file)
+    #if image_type or image.filename.lower().endswith('heic'):
+    if True : #тут можно проверить изображение ли это вообще...см.строку выше
+     
+        with Image.open(BytesIO(await image.read())) as img:
+            if img.mode != 'RGB':
+                img = img.convert("RGB")
+            try:
+                pok = prod.look_to_file(img,base.dict())#отправляем изображение и то, что требуется вернуть
+            except:
+                pok = wrong
+                #print(pok)
+			
+			#можно сохранить полученное изображение в arh
+            #otvet = [f'{round(pok["net1"][key],2)}_{round(pok["net2"][key],2)} '   for key in keys]
+            #otvet = '__'.join(otvet)
+            #print(otvet)
+
+            #need_moderation = pok['need_moderation']
+            #current_datetime = datetime.now()
+            #str_current_datetime = current_datetime.strftime("%Y-%m-%d %H-%M-%S.%f")
+        
+            #filename = f"need_mod_{need_moderation}__{otvet}__{str_current_datetime}.jpg"
+            
+            #filename = os.path.join('arh',filename)
+            #img.save(filename)
+            #print('Сохранено  ',filename)
+
+			
+            gc.collect() #почистимся 
+            return pok
+    else:
+         return None
+
+@app.post("/test")
+async def analyze_image(image: UploadFile = File(...)):
+    with Image.open(BytesIO(await image.read())) as img:
+         current_datetime = datetime.now()
+         str_current_datetime = current_datetime.strftime("%Y-%m-%d %H-%M-%S")
+         filename = f"inp_{str_current_datetime}.jpg"
+         filename = os.path.join('arh', filename)
+         #img.save(filename)
+         return {"width": img.width, "height": img.height}
+
+
+
+txt = 'Этот адрес url не предназначен для открытия в браузере. Используйте post запрос в формате requests.post(url, files=file) где - file - это бинарный файл с изображением для распознавания'
+
+
+
+@app.get("/ps")
+async def not_use():
+   
+   
+    return txt
+
+@app.get("/test")
+async def not_use():
+   
+    return txt
+
+app.include_router(router)
